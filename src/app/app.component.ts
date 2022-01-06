cfef09870a125b53ed11ea7590ec0f75c6e4d10f
Djilani CARDINEAU <djilani.cardineau@epitech.eu>
2022-01-06 23:35:49 +0100

    electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'download-manga-electron';
+  title = 'download-manga-electronqdsd';
 }
